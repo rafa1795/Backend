ff075731ad3384b6e09adcb5cd6d6adecae3e63f
rafa1795 <rafaelsantagata11@gmail.com>
2024-06-05 15:19:29 -0300

    Update commander.js

--- src/utils/commander.js
+++ src/utils/commander.js
@@ -1,7 +1,6 @@
 const {Command} = require("commander");
 const program = new Command();
 
-//1 - Comando // 2 - La descripcion // 3 - Valor por default
 
 program
     .option("--mode <mode>", "entorno de trabajo", "produccion")
