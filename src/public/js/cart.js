ed107782f3a2f854322eeb73b55ed68d9deb849a
Rafael S <rafaelsantagata11@gmail.com>
2024-09-03 11:46:44 -0300

    Estilos

--- src/public/js/cart.js
+++ src/public/js/cart.js
@@ -13,37 +13,40 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     async function agregarProductoAlCarrito(event) {
-    const productId = this.dataset.productId;
-    const cartId = localStorage.getItem("cartId");
-
-    console.log("Product ID:", productId);
-    console.log("Cart ID:", cartId);
-
-    if (!productId) {
-        console.error("Error: el productId no se encuentra en el dataset del botón.");
-        return;
-    }
-
-    if (!cartId) {
-        console.error("Error: no se encontró el cartId en el localStorage.");
-        return;
-    }
-
-    try {
-        const response = await fetch(`/api/carts/${cartId}/product/${productId}`, {
-            method: "POST",
-        });
-
-        if (!response.ok) {
-            throw new Error("Error al agregar producto al carrito");
+        const productId = this.dataset.productId;
+        const cartId = localStorage.getItem("cartId");
+    
+        console.log("Product ID:", productId);
+        console.log("Cart ID:", cartId);
+    
+        if (!productId) {
+            console.error("Error: el productId no se encuentra en el dataset del botón.");
+            return;
         }
-
-        const data = await response.json();
-        console.log("Producto agregado al carrito:", data);
-    } catch (error) {
-        console.error("Error al agregar producto al carrito:", error);
+    
+        if (!cartId) {
+            console.error("Error: no se encontró el cartId en el localStorage.");
+            return;
+        }
+    
+        try {
+            const response = await fetch(`/api/carts/${cartId}/product/${productId}`, {
+                method: "POST",
+            });
+    
+            if (!response.ok) {
+                throw new Error("Error al agregar producto al carrito");
+            }
+    
+            const data = await response.json();
+            console.log("Producto agregado al carrito:", data);
+    
+            alert("Producto agregado al carrito exitosamente!");
+    
+        } catch (error) {
+            console.error("Error al agregar producto al carrito:", error);
+        }
     }
-}
 
 async function confirmarCompra(event) {
     event.preventDefault();
